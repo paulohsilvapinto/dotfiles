67d9d8f49c8c98af00da24c0aad15203a7fd0151
Paulo Henrique Silva Pinto <paulohsilvapinto@gmail.com>
2021-12-22 18:05:18 +0000

    Update make script

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -2,3 +2,18 @@
 function mkd() {
     mkdir -p "$@" && cd "$_";
 }
+
+
+# Navigates to the directory even if the path contains a filename
+function cd() {
+    if [ $# -eq 0 ] ; then
+        # no arguments
+        builtin cd
+    elif [ -d $1 ] ; then
+        # argument is a directory
+        builtin cd "$1"
+    else
+        # argument is not a directory
+        builtin cd "$(dirname $1)"
+    fi
+}
--- make.sh
+++ make.sh
@@ -5,27 +5,43 @@
 ############################
 
 ########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc gitconfig"    # list of files/folders to symlink in homedir
-
+base_path="$(dirname "$0")"             # Script location
+bkp_dir=~/dotfiles_bkp                  # Dotfiles backup directory
+omz_custom_dir=.oh-my-zsh/custom        # Oh-my-zsh custom directory 
+plugins_path=$omz_custom_dir/plugins    # Oh-my-zsh plugins directory
+dotfiles="vimrc zshrc gitconfig"        # list of files/folders to symlink in homedir
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+# Go to the Oh-my-zsh plugins directory
+cd $base_path/$plugins_path
+
+# Download submodules
+echo "Downloading submodules"
+for d in */ ; do
+    [ -L "${d%/}" ] && continue # ignore symlinks
+    cd $d && git submodule upgrade --init --recurse && cd ..
+done
+
+# Replace zshrc.sh file with custom git prompt
+cat zsh-git-prompt-custom-zshrc.sh > zsh-git-prompt/zshrc.sh
+
+# Create oh-my-zsh custom symlink
+echo "Creating Oh-my-zsh custom symlink"
+ln -s $base_path/$omz_custom_dir ~$USER/$omz_custom_dir
+
+echo "Creating $bkp_dir for backup of any existing dotfiles in ~"
+mkdir -p $bkp_dir
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+echo "Changing directory to " ~$USER
+cd ~$USER
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
+    echo "Moving any existing dotfiles from ~$USER to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $base_path/.$file ~/.$file
 done
+
+
